411acb2192c6b07cdb501487312baf054f864777
season1618 <seasonminority314@gmail.com>
2021-05-22 17:38:36 +0900

    introduce WinMain

--- browser.cpp
+++ browser.cpp
@@ -140,10 +140,39 @@ void HTML_parser_test(int parent_id, int depth){
         HTML_parser_test(child_id, depth + 1);
     }
 }
-int main(void){
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+    /*HWND hwnd;
+	WNDCLASS winc;
+
+	winc.style = CS_HREDRAW | CS_VREDRAW;
+	winc.lpfnWndProc = DefWindowProc;
+	winc.cbClsExtra	= winc.cbWndExtra = 0;
+	winc.hInstance = hInstance;
+	winc.hIcon = LoadIcon(NULL , IDI_APPLICATION);
+	winc.hCursor = LoadCursor(NULL , IDC_ARROW);
+	winc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	winc.lpszMenuName = NULL;
+	winc.lpszClassName = TEXT("KITTY");
+
+	if (!RegisterClass(&winc)) return 0;
+
+	hwnd = CreateWindow(
+			TEXT("KITTY"), TEXT("Kitty on your lap"),
+			WS_OVERLAPPEDWINDOW,
+			100, 100, 200, 200, NULL, NULL,
+			hInstance, NULL
+	);
+
+	if (hwnd == NULL) return 0;
+
+	ShowWindow(hwnd , SW_SHOW);*/
+    MessageBox(NULL, "", "", MB_OK);
+    return 0;
+}
+/*int main(void){
     //char url[] = "https://www.google.com";
     char url[] = "https://ja.wikipedia.org/wiki/Uniform_Resource_Locator";
     HINTERNET hRequest = HttpRequest(url);
     HTML_parser(hRequest, 0);
     HTML_parser_test(0, 0);
-}
+}*/
